d0d7ab471da95854a7efa800860d6cc635103931
tpokora <pokora.tomek@gmail.com>
2015-03-15 18:53:15 +0100

    Basic form for adding task with datePickerDialog

--- app/src/main/java/mayon/org/todo/AddTask.java
+++ app/src/main/java/mayon/org/todo/AddTask.java
@@ -1,17 +1,45 @@
 package mayon.org.todo;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.provider.CalendarContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 public class AddTask extends ActionBarActivity {
+
+    private TextView addTaskTopicLabel;
+    private EditText addTaskTopicEditText;
+    private Button datePickButton;
+    private DatePicker datePicker;
+
+    private int year, month, day;
+
+    static final int DATE_DIALOG_ID = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        addTaskTopicLabel = (TextView) findViewById(R.id.addTaskFormTopicLabel);
+        addTaskTopicEditText = (EditText) findViewById(R.id.addTaskFormEditText);
+        datePickButton = (Button) findViewById(R.id.dataPickButton);
+        datePicker = new DatePicker(this);
+
+        setDateOnDatePicker();
+        addDatePickerButtonListener();
     }
 
 
@@ -36,4 +64,49 @@ public class AddTask extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void setDateOnDatePicker() {
+        final Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+        datePicker.init(year, month, day, null);
+    };
+
+    public void addDatePickerButtonListener() {
+        datePickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, datePickerListener,
+                        year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int selectedYear,
+                                      int selectedMonth, int selectedDay) {
+                    year = selectedYear;
+                    month = selectedMonth;
+                    day = selectedDay;
+
+                    datePickButton.setText(new StringBuilder().append(day)
+                        .append("-").append(month).append("-").append(year).append(""));
+
+
+                }
+            };
+
+
 }
